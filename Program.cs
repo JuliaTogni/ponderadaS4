7d64126ac81ee3cbb34916e6f36b6f531cce6b90
JuliaTogni <99190451+JuliaTogni@users.noreply.github.com>
2024-05-13 08:51:09 -0300

    feat: mudanças

--- Program.cs
+++ Program.cs
@@ -6,12 +6,11 @@ using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona HatCoMetrics como um singleton para que uma única instância seja usada durante toda a execução da aplicação
 builder.Services.AddSingleton<HatCoMetrics>();
 
 var app = builder.Build();
 
-// Endpoint para acionar a simulação de métricas
+// Endpoint
 app.MapGet("/metrics", (HatCoMetrics metrics) =>
 {
     metrics.SimulateMetrics();
@@ -27,7 +26,6 @@ public partial class Program
     static ObservableCounter<int> s_coatsSold = s_meter.CreateObservableCounter<int>("hatco.store.coats_sold", () => s_rand.Next(1, 10));
     static Random s_rand = new Random();
 
-    // Esta parte parece ser um resíduo de código de console e deve ser removida ou comentada se não estiver sendo usada.
     /*
     public static void Main(string[] args)
     {
